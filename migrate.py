e73d9d36050fd5ee67a646b27d83151d8b0f6f04
Mike Perrin <mike@alvo.co.uk>
2020-03-05 14:01:28 +0000

    introduced JoplinConnection

--- migrate.py
+++ migrate.py
@@ -2,7 +2,9 @@ import urllib.request
 import json 
 from sys import argv
 
-#ToDo - create a backup before we start! https://discourse.joplinapp.org/t/best-method-to-backup-notes/1135
+#ToDo
+# create a backup before we start! https://discourse.joplinapp.org/t/best-method-to-backup-notes/1135
+# pull out the token somewhere safe
 
 replacements={
     'evernote:///view/237202231/s7/82254d2d-f19d-49b6-9ab6-7c57626e6897/82254d2d-f19d-49b6-9ab6-7c57626e6897/':':/79bfb652ff4c41d89b42b6d92cc3469d', #Home Page
@@ -14,57 +16,54 @@ replacements={
     'evernote:///view/926571/s7/83df728f-6641-489c-aacf-c2db30943cb2/83df728f-6641-489c-aacf-c2db30943cb2/':':/85c4d82e80be4af8ba308d668ff5be44' #Wifi keys
 }
 
-def replaceTextInNote(note, textToReplace, replacement):
-    noteBody = note["body"]
-    noteId = note["id"]
-    noteTitle = note["title"]
-    opener = urllib.request.build_opener(urllib.request.HTTPHandler)
-    print(f'Replacing:<{textToReplace}> with:<{replacement}> in the note with title:<{noteTitle}>')
-    newNoteBody = noteBody.replace(textToReplace, replacement)
-    payload = {'body': newNoteBody}
-    putUrl = f'{baseUrl}notes/{noteId}?{tokenUrlPart}'
-    payloadAsBytes = bytes(json.dumps(payload), encoding="utf-8")
-    #ToDo - switch to using requests https://2.python-requests.org/
-    request = urllib.request.Request(putUrl, data=payloadAsBytes)
-    request.add_header('Content-Type', 'application/json')
-    request.get_method = lambda: 'PUT'
-    if commitChanges:
-        opener.open(request)
+class JoplinConnection():
+    def __init__(self, joplin_token):
+        self.joplin_token = joplin_token
+        self.baseUrl = 'http://localhost:41184/'
+        self.tokenUrlPart = f'&token={self.joplin_token}'
+        self.notesUrl = f'{self.baseUrl}notes?token{self.joplin_token}'
+        self.commitChanges = False #Set to True to commit changes to notes
 
-def searchAndReplace(textToReplace, replacement):
-    #ToDo - can I restrict this search to a single notebook for safety? Doesn't look like it: https://joplinapp.org/#searching
-    searchUrl = f'{baseUrl}search?query={textToReplace}{tokenUrlPart}&fields=id,body,title'
-    searchResultsJson = urllib.request.urlopen(searchUrl).read()
-    searchResults = json.loads(searchResultsJson)
-    print(f'Searching for :<{textToReplace}> found:<{len(searchResults)}> notes')
-    for note in searchResults:
-        replaceTextInNote(note, textToReplace, replacement)
+    def ping(self):
+        url = f'{self.baseUrl}ping?{self.tokenUrlPart}'
+        contents = urllib.request.urlopen(url).read()
+        print(f'Joplin server at:<{url}> returned:<{contents}>')
 
-def ping():
-    url = f'{baseUrl}ping?{tokenUrlPart}'
-    contents = urllib.request.urlopen(url).read()
-    print(f'Joplin server at:<{url}> returned:<{contents}>')
+    def __replaceTextInNote(self, note, textToReplace, replacement):
+        noteBody = note["body"]
+        noteId = note["id"]
+        noteTitle = note["title"]
+        opener = urllib.request.build_opener(urllib.request.HTTPHandler)
+        print(f'Replacing:<{textToReplace}> with:<{replacement}> in the note with title:<{noteTitle}>')
+        newNoteBody = noteBody.replace(textToReplace, replacement)
+        payload = {'body': newNoteBody}
+        putUrl = f'{self.baseUrl}notes/{noteId}?{self.tokenUrlPart}'
+        payloadAsBytes = bytes(json.dumps(payload), encoding="utf-8")
+        #ToDo - switch to using requests https://2.python-requests.org/
+        request = urllib.request.Request(putUrl, data=payloadAsBytes)
+        request.add_header('Content-Type', 'application/json')
+        request.get_method = lambda: 'PUT'
+        if self.commitChanges:
+            opener.open(request)
 
-def main():
-    #ping()
-    for key, value in replacements.items():
-        old = key
-        new = value
-        searchAndReplace(old, new)
-    print('finished!')
-
-print (len(argv))
-print (argv)
-
+    def searchAndReplace(self, textToReplace, replacement):
+        #ToDo - can I restrict this search to a single notebook for safety? Doesn't look like it: https://joplinapp.org/#searching
+        searchUrl = f'{self.baseUrl}search?query={textToReplace}{self.tokenUrlPart}&fields=id,body,title'
+        searchResultsJson = urllib.request.urlopen(searchUrl).read()
+        searchResults = json.loads(searchResultsJson)
+        print(f'Searching for :<{textToReplace}> found:<{len(searchResults)}> notes')
+        for note in searchResults:
+            self.__replaceTextInNote(note, textToReplace, replacement)        
 
 if len(argv) <= 1:
-     print('Useage: migrate.py <joplinToken>')
-     exit
-token = argv[1]
-baseUrl = 'http://localhost:41184/'
-tokenUrlPart = f'&token={token}'
-notesUrl = f'{baseUrl}notes?token{token}'
-commitChanges = False #Set to True to commit changes to notes
+    exit('Useage: migrate.py <joplinToken>')
+joplin_token = argv[1]
+joplin_connection = JoplinConnection(joplin_token)
+joplin_connection.ping()
+for key, value in replacements.items():
+    old = key
+    new = value
+    joplin_connection.searchAndReplace(old, new)
+print('finished!')
 
-ping()
 # main()
